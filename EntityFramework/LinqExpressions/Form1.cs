b5129f9bd6d9c3c40a431b0099de352a5086c7bb
adabusra <busraa.adaa@gmail.com>
2017-06-28 23:53:38 +0300

    Linq

---
+++ EntityFramework/LinqExpressions/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqExpressions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            northwindDataContext nt = new northwindDataContext();
+            dataGridView1.DataSource = from c in  nt.Customers join o in nt.Orders on c.CustomerID equals o.CustomerID select new
+              {
+                     o.Customer,
+                     o.CustomerID,
+                     o.Employee,
+                     c.ContactName,
+                     c.ContactTitle,
+                     c.Country,
+                     c.Address
+              };
+
+        }
+    }
+}
---
+++ EntityFramework/LinqExpressions/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqExpressions
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            northwindDataContext nt = new northwindDataContext();
+            dataGridView1.DataSource = from o in nt.Orders  join emp in nt.Employees on o.EmployeeID equals emp.EmployeeID  select new
+                                       {
+                                            emp.EmployeeID,
+                                            emp.Employees,
+                                            emp.EmployeeTerritories,
+                                          o.Customer,
+                                          o.CustomerID
+                                       };
+        }
+    }
+}
